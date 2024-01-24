c95cca53bf09f8807e488f927dfc6d49d24a1f2f
sahunitin023 <b521039@iiit-bh.ac.in>
2024-01-24 20:39:02 +0530

    Project Done

---
+++ main.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+
+# followings = []
+# followers = []
+
+
+def get_followings():
+    with open('following.html', 'r', encoding='utf-8') as file:
+        following_contents = file.read()
+
+    soup = BeautifulSoup(following_contents, 'html.parser')
+    # following_span_tag = soup.find(name='span', class_='_ap3a _aaco _aacw _aacx _aad7 _aade')
+    followings_list = [tag.getText() for tag in soup.find_all(name='span', class_='_ap3a _aaco _aacw _aacx _aad7 _aade')]
+    return followings_list
+
+
+def get_followers():
+    with open('followers.html', 'r', encoding='utf-8') as file1:
+        follower_content = file1.read()
+
+    soup1 = BeautifulSoup(follower_content, 'html.parser')
+    followers_list = [tag.getText() for tag in soup1.find_all(name='span', class_='_ap3a _aaco _aacw _aacx _aad7 _aade')]
+    return followers_list
+
+
+followers = get_followers()
+followings = get_followings()
+
+not_following_accounts = [acc for acc in followings if acc not in followers]
+print(not_following_accounts)
+
